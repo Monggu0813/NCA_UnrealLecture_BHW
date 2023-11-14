a1b49a2ebdf041c07a4ff68c431c606bde4e359f
Hyowon <vhdvhd1478@naver.com>
2023-11-14 17:05:08 +0900

    First

---
+++ Source/NCA_BHW/NCA_BHW.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NCA_BHW.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NCA_BHW, "NCA_BHW" );
